4d008e999ecbcb31e5c6d88489263d22156c99c8
Taeyeyta\taeyeyta1996 <taeytaey_2539@hotmail.com>
2017-12-15 20:33:11 +0700

    v

---
+++ OpenOrderFramework/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace OpenOrderFramework.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contact() {
+           
+            return View();
+        }
+    }
+}
---
+++ OpenOrderFramework/Models/IdentityModels.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace OpenOrderFramework.Models {
+    public class ApplicationUser : IdentityUser {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager) {
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser> {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false) {
+        }
+
+
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Catagorie> Catagories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        static ApplicationDbContext() {
+            Database.SetInitializer<ApplicationDbContext>(new ApplicationDbInitializer());
+        }
+
+        public static ApplicationDbContext Create() {
+            return new ApplicationDbContext();
+        }
+    }
+}
